84830e0e139941664bb4f83a25c4264527507527
Junayd Bhoyroo <contact@junaydb.com>
2024-12-17 01:56:50 +0000

    add linux machine's configs

---
+++ fish/linux/config.fish
@@ -0,0 +1,11 @@
+# enable vim bindings
+fish_vi_key_bindings
+
+# aliases
+alias lsa='ls -A'
+alias vim='nvim'
+
+# blinking block cursor 
+set fish_vi_force_cursor
+set fish_cursor_default block blink
+set fish_cursor_insert block blink
---
+++ fish/linux/functions/cl.fish
@@ -0,0 +1,5 @@
+# clear the screen and scroll-back buffer
+function cl
+	clear
+	printf '\033[3J'
+end
